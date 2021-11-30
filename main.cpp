1845d95c1398abcc80c14652e2e8333c4c2db528
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-11-30 18:53:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    char a;
+    cin>>a;
+    int ris= verifica (a);
+    if(ris){
+        cout<< convert(a)<<endl;
+    }else{
+        cout<<"errore";
+    }
 
-int main(){
-  return 0;
+    return 0;
 }
