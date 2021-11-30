49e0590905ebfa6219bf3c4090c4b4ea4164e397
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-11-30 18:55:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,18 @@
+#include "lib.h"
 
+bool verifica(char a){
+
+    if((a>=65 && a<=90) || (a>=97 && a<=122)){
+        return true;
+    }else{
+        return false;
+    }
+}
+char convert (char a){
+    if(a>=65 && a<=90){
+        a=a+32;
+    }else if (a>=97 && a<=122){
+        a=a-32;
+        return a;
+    }
+}
